0186dfe0cbd62c15c4186daca8fe62e434b6bc54
Matheus Cristofoli <matfrietasc@gmail.com>
2022-08-23 01:08:30 -0400

    Added an about page component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Error from './pages/Error';
 import Profile from './pages/Profile';
+import About from './pages/About';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
         <Route path='/:name' element={<Profile />} />
         <Route path='*' element={<Error />} />
       </Route>
