3ebaa80ab1a8daf34c599f1c787117f9fc943ad2
Matheus Cristofoli <matfrietasc@gmail.com>
2022-08-25 04:39:12 -0400

    Adding Hashrouter

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.sass';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path='/*' element={<App />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
