abbee368bd24206481896083b59b95a2c72f1822
Matheus Cristofoli <matfrietasc@gmail.com>
2022-08-25 04:33:36 -0400

    fix a small bug in the home/page

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div className='page-container'>
       <Routes>
         <Route path='/' element={<Layout />}>
-          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/profile/:name' element={<Profile />} />
           <Route path='*' element={<Error />} />
